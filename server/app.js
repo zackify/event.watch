3757f64678781354d36a775dbdc1f12bce1e35fc
Zach <zackify@gmail.com>
2015-08-17 20:45:19 -0700

    added postgres

--- server/app.js
+++ server/app.js
@@ -1,9 +1,11 @@
 var app = require('koa')();
+var koaPg = require('koa-pg')
 var routes = require('./routes');
 
 app
-  .use(routes.api.routes())
-  .use(routes.main.routes())
-  .use(routes.main.allowedMethods());
+.use(routes.api.routes())
+.use(routes.main.routes())
+.use(routes.main.allowedMethods())
+.use(koaPg('postgres://root:root@localhost:5432/eventwatch'))
 
 app.listen(3000);
---
+++ server/models/index.js
@@ -0,0 +1,7 @@
+var User = require('./user')
+var Event = require('./event')
+
+module.exports = {
+  User: User,
+  Event: Event
+}
