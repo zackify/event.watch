4c271df0ca4c07bed5b24c347ae9fb3e076e00ff
Zach <zackify@gmail.com>
2015-08-20 00:14:42 -0700

    set timezone correctly hopefully

--- server/api/helpers/parseTime.js
+++ server/api/helpers/parseTime.js
@@ -21,7 +21,7 @@ function returnHour(time, type){
   return number
 }
 
-function parseNextDate(time){
+function parseNextDate(time, timezone){
   var dayOfWeek = nextDates[time[1]]
   if(!time[2]) return false
 
@@ -33,15 +33,14 @@ function parseNextDate(time){
   else if(time[3].match(/[0-9]/)) var hour = returnHour(time[3])
   else return false
 
-  return moment().day(dayOfWeek).startOf('day').hour(hour).format()
+  return moment().day(dayOfWeek).startOf('day').hour(hour).tz(timezone).format()
 }
 
 module.exports = function(text, timezone){
-  moment().tz(timezone).format()
   var afterTime = text.match(/(?:pm|am)\s(.*)/) //the part after the time passed in
   if(afterTime) var time = text.replace(afterTime, '').toLowerCase().split(' ') //the time part of the string
   else var time = text.toLowerCase().split(' ')
 
-  if(time[0] == 'next') return parseNextDate(time)
+  if(time[0] == 'next') return parseNextDate(time, timezone)
 
 }
