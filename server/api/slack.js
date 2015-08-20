1938d387c8be8a65b0bb72b2d8be8b3cb65b15a2
Zach <zackify@gmail.com>
2015-08-20 00:06:12 -0700

    added message if wrong event data format

--- server/api/slack.js
+++ server/api/slack.js
@@ -5,7 +5,9 @@ module.exports = function *() {
   var user = yield getUser.call(this)
   if(!user) return this.body = "You must add your slack token ("+this.request.body.token+") to your <https://event.watch/account|event.watch account> before adding an event!"
 
-  var event_time = parseTime(this.request.body.text)
+  var event_time = parseTime(this.request.body.text, user.timezone)
+  if(!event_time) return this.body = "Oops, you passed an invalid date format. Check out <https://event.watch/account|our docs> if you're confused!"
+
   var channel_id = this.request.body.channel_id
 
   this.body = "So you want to add an event on " + event_time + ", ey " +  this.request.body.user_name + '?'
--- test/server/api/slack.js
+++ test/server/api/slack.js
@@ -1,16 +1,29 @@
 var request = require('supertest');
 var app = require('../../../server/app');
+var agent = require('supertest-koa-agent');
+agent(app)
 
 describe('Slack endpoint', function() {
   describe('#post', function () {
     it("should return error if token doesn't belong to a user", function (done) {
       request(app.listen())
         .post('/api/v1/slack')
-        .field('token', 'blah')
+        .send({"token": "blah"})
         .end(function(err, res){
           if (!res.text.match(/You must add your slack token/)) done("didn't throw error")
           else done(err)
         })
     });
+    //setup a test database so i can actually run this on circle ci
+    // it("should return an error if invalid date format", function (done) {
+    //   request(app.listen())
+    //     .post('/api/v1/slack')
+    //     .set('Accept', 'application/json')
+    //     .send({"token": "test_db_token", text: "next blah"})
+    //     .end(function(err, res){
+    //       if (!res.text.match(/invalid date format/)) done("didn't throw error")
+    //       else done(err)
+    //     })
+    // });
   });
 });
