f20b9244e1317c8cabe71f7a122593ef8190f5a9
Zach <zackify@gmail.com>
2015-08-21 20:41:39 -0700

    added sentry error handling

--- server/app.js
+++ server/app.js
@@ -1,8 +1,10 @@
 var app = require('koa')();
 var json = require('koa-json');
+var raven = require('koa-raven');
 
 var main = require('./routes/main');
 var api = require('./routes/api');
+raven(app)
 
 app
 .use(json())
