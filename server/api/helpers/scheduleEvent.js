a7600e25705d2d6138e12ad486ba5fa047880a74
Zach <zackify@gmail.com>
2015-08-27 20:24:52 -0700

    testing more on heroku...

--- server/api/helpers/scheduleEvent.js
+++ server/api/helpers/scheduleEvent.js
@@ -1,14 +1,14 @@
 var schedule = require('node-schedule')
 var request = require('superagent')
-
+//'2015-08-28T03:15:00+00:00'
 module.exports = function(data){
-  schedule.scheduleJob('2015-08-28T03:15:00+00:00', function(){
+  schedule.scheduleJob(Date.now(), function(){
     console.log('the request is being called')
     request
     .post(data.hook)
     .send({text: data.text, channel: data.channel})
     .end(function(err,res){
-      console.log(err,res)
+      console.log(res.data)
     })
   })
 }
